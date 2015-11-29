c8b82747995c08c435755063534685bbf8453c44
GravelChi <achogarth@gmail.com>
2015-11-29 15:12:38 -0600

    comments in serverworker to show where logic needs to go

--- src/starTrekReference/ServerWorker.java
+++ src/starTrekReference/ServerWorker.java
@@ -1,11 +1,16 @@
 package starTrekReference;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+
 import javax.swing.SwingWorker;
 
 public class ServerWorker extends SwingWorker {
 
 	boolean accepting;
 	int engagedConnections;
+	FileReader reader;
+	FileWriter writer;
 	
 	public ServerWorker () {
 		accepting = true;
@@ -16,10 +21,51 @@ public class ServerWorker extends SwingWorker {
 	protected Object doInBackground() throws Exception {
 		// TODO This is where the server logic will go
 		while (accepting){//server is accepting new connections
+			//before we receive shutdown from server gui
+			
+			//listen for incoming packet
+			//if engage , reply to setup connection by completing handshake
+			//handshake section
+			
+			
+			
+			//end handshke section
+			
+			//loop to recieve/send data
+			//while(!stahp){
+			
+			
+//			 s.setSoTimeout(1000);   // set the timeout in millisecounds.
+//
+//		        while(true){        // recieve data until timeout
+//		            try {
+//		                s.receive(dp);
+//		                String rcvd = "rcvd from " + dp.getAddress() + ", " + dp.getPort() + ": "+ new String(dp.getData(), 0, dp.getLength());
+//		                System.out.println(rcvd);
+//		            }
+//		            catch (SocketTimeoutException e) {
+//		                // timeout exception.
+//		                System.out.println("Timeout reached!!! " + e);
+//		                s.close();
+//		            }
+//		        }
+			
+			
+			
+			//}end while
+			
 			
 			
 			
 		}
+		
+		//after we receive shutdown from gui
+		//finish off all active connections
+		
+		
+		
+		
+		
 		
 		//cleanup
 		
